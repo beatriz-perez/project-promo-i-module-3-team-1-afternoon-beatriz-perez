c062ea2583848727f58cacf9ec0c446a10fdf712
beatriz-perez <beatriz.pfp@gmail.com>
2020-04-21 16:27:30 +0200

    update docs folder

---
+++ docs/build/precache-manifest.5931a5236dcfce8d8839b2939a003102.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd6fe38cbc334ecab4e93a496870796a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a47fd371132986e3ac64",
+    "url": "./static/css/main.f8e36432.chunk.css"
+  },
+  {
+    "revision": "64e9d8d27c6051dbdd1e",
+    "url": "./static/js/2.988e1492.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.988e1492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a47fd371132986e3ac64",
+    "url": "./static/js/main.42fefef1.chunk.js"
+  },
+  {
+    "revision": "39698c42a76751fa35c9",
+    "url": "./static/js/runtime-main.f74246b4.js"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/awesome_logo.0118e311.svg"
+  },
+  {
+    "revision": "0dc830c4168743ea2bd664a6ed7613ac",
+    "url": "./static/media/default-avatar.0dc830c4.gif"
+  },
+  {
+    "revision": "009a2ec86a0240002cbbfad8e7772485",
+    "url": "./static/media/logo-localUnicorns.009a2ec8.png"
+  }
+]);
